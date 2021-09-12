2a25654df55b44765ff324491b78efb92c2b8141
nehavenkat1 <nehavenky@yahoo.com>
2021-09-12 17:34:33 +0530

    new files added

--- project1/src/user/pages/Users.js
+++ project1/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import UserList from '../components/UserList'
+
 const Users = () => {
-    return <h1> user page </h1>
+    const USERS = [
+        {
+            id: 'uid1',
+            image: 'https://www.desicomments.com/dc3/19/439319/4393191.jpg',
+            name: 'John',
+            places: 3
+        }
+    ] 
+
+    return <UserList items={USERS} />
 }
 
 export default Users
