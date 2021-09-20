9114563ef680b53a5c74067a1f9926d87fa3459d
nehavenkat1 <nehavenky@yahoo.com>
2021-09-20 17:57:30 +0530

    added new functionality

--- project1/src/places/pages/UpdatePlace.js
+++ project1/src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'
 
 import Input from '../../shared/components/FormElements/Input'
@@ -40,20 +40,39 @@ const DUMMY_PLACES = [
 
 
 const UpdatePlace = props => {
+    const [isLoading, setIsLoading] = useState(true)
+
     const placeId = useParams().placeId
     
-    const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeId)
-
-    const [formState, inputHandler] = useForm({
+    const [formState, inputHandler, setFormData] = useForm({
         title: {
-            value: identifiedPlace.title,
-            isValid: true
+            value: '',
+            isValid: false
         },
         description: {
-            value: identifiedPlace.description,
-            isValid: true
+            value: '',
+            isValid: false
         }
-    }, true)
+    }, false)
+
+    const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeId)
+
+    useEffect(() => {
+        setFormData({
+            title: {
+                value: identifiedPlace.title,
+                isValid: true
+            },
+            description: {
+                value: identifiedPlace.description,
+                isValid: true
+            }
+        },
+        true
+        )
+        setIsLoading(false)
+    }, [setFormData, identifiedPlace])
+    
 
     const placeUpdateSubmitHandler = event => {
         event.preventDefault()
@@ -67,6 +86,15 @@ const UpdatePlace = props => {
             </div>
         )
     }
+
+    if(isLoading) {
+        return(
+            <div className="center">
+                <h2>Loading...</h2>
+            </div>
+        )
+    }
+
     return <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
         <Input 
             id="title"
