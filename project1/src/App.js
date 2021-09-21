5e145b75cffb2c54e48f33420701364876f7b1a3
nehavenkat1 <nehavenky@yahoo.com>
2021-09-21 18:24:54 +0530

    new functionality added

--- project1/src/App.js
+++ project1/src/App.js
@@ -9,8 +9,10 @@ import {
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
 import UserPlaces from './places/pages/UserPlaces'
+import Auth from './user/pages/Auth'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
 import UpdatePlace from './places/pages/UpdatePlace'
+
 
 function App() {
   return (
@@ -29,6 +31,9 @@ function App() {
             </Route>
             <Route path="/places/:placeId">
               <UpdatePlace />
+            </Route>
+            <Route path="/auth">  
+              <Auth />
             </Route>
             <Redirect to='/' />
           </Switch>
--- project1/src/places/components/PlaceItem.js
+++ project1/src/places/components/PlaceItem.js
@@ -8,11 +8,25 @@ import './PlaceItem.css'
 const PlaceItem = props => {
     const [showMap, setShowMap] = useState(false)
 
+    const [showconfirmModal, setShowConfirmModal] = useState(false)
+
     const openMapHandler = () => setShowMap(true)
 
     const closeMapHandler = () => setShowMap(false) 
 
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true)
+    }
     
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false)
+    }
+
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false)
+        console.log('deleting...')
+    }
+
     return (
         
         <React.Fragment>
@@ -28,6 +42,20 @@ const PlaceItem = props => {
                     <h2>THE MAP!</h2>
                 </div>
             </Modal>  */ }
+            { /*
+            <Modal 
+                show={showconfirmModal}
+                onCancel={cancelDeleteHandler}
+                header="Are you sure?" 
+                footerClass="place-item__modal-actions" 
+                footer={
+                <React.Fragment>
+                    <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+                    <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+                </React.Fragment>
+            }>
+                <p>If you delete it you can't get it back.</p>
+        </Modal> */}
         <li className="place-item">
         <Card className="place-item__content">
         <div className="place-item__image">
@@ -41,7 +69,7 @@ const PlaceItem = props => {
         <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}> VIEW ON MAP</Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
         </div>
         </Card>
     </li>
